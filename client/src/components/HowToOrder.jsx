c2639ab55f4e3a3bd84560522f76bb097d4e0eec
Yash Nagpaul <yash.nagpaul@gmail.com>
2020-09-04 11:15:56 -0700

    made changes to How-to-order.jsx

--- client/src/components/HowToOrder.jsx
+++ client/src/components/HowToOrder.jsx
@@ -6,19 +6,47 @@ import { Link } from "react-router-dom";
 export default function HowToOrder() {
   return (
     <div className="how-to-order">
-      <h1>
-        How would you
-        <br />
-        like to order?
-      </h1>
-      <div className="how-to-order__icon-container">
-        <Link to="/talk">
-          <img className="how-to-order__icon" src={callIcon} alt="" />
-        </Link>
-        <Link to="/chat">
-          <img className="how-to-order__icon" src={messageIcon} alt="" />
-        </Link>
-      </div>
+      <section className="how-to-order__left-side">
+        <div>1</div>
+        Enter your information below.
+        <input
+          className="how-to-order__text-input"
+          type="text"
+          placeholder="Name"
+        ></input>
+        <input
+          className="how-to-order__text-input"
+          type="text"
+          placeholder="Address"
+        ></input>
+        <input
+          className="how-to-order__text-input"
+          type="text"
+          placeholder="Phone Number"
+        ></input>
+      </section>
+
+      <section className="how-to-order__right-side">
+        <div>2</div>
+        <h3>
+          Please select an option below and someone can help you place your
+          order.
+        </h3>
+        <div className="how-to-order__icon-container">
+          <Link to="/talk">
+            <div className="how-to-order__call-btn">
+              <img className="how-to-order__icon" src={callIcon} alt="" />
+              Call
+            </div>
+          </Link>
+          <Link to="/talk">
+            <div className="how-to-order__icon">
+              <img className="how-to-order__icon" src={messageIcon} alt="" />
+              Chat
+            </div>
+          </Link>
+        </div>
+      </section>
     </div>
   );
 }
