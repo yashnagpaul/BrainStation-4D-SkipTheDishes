e6651421eff76fae5089e39602dc7dee5b4de2ef
Yash Nagpaul <yash.nagpaul@gmail.com>
2020-09-04 10:43:23 -0700

    created a chat component

---
+++ client/src/components/Chat.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Chat() {
+  return (
+    <div className="chat">
+      <div className="chat__map-locations-container">
+        <img src="" alt="" />
+        <img src="" alt=""></img>
+      </div>
+      <div className="chat__chat-box-container"></div>
+    </div>
+  );
+}
+
+export default Chat;
